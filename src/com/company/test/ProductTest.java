59f8bb5506aa322c23da8909904906cad3d409f0
Chao <nickzhang0608@gmail.com>
2020-09-29 10:37:04 +0800

    first commit

---
+++ src/com/company/test/ProductTest.java
@@ -0,0 +1,35 @@
+package com.company.test;
+
+import com.company.model.Product;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+
+public class ProductTest {
+    private Product product;
+
+    @Before
+    public void setUp() {
+        product = new Product("iphone");
+    }
+
+    @Test
+    public void testConstructor() {
+        assertEquals("iphone", product.getName());
+    }
+
+    @Test
+    public void testSetter() {
+        String testName = "iphone 11";
+        product.setName(testName);
+        assertEquals(testName, product.getName());
+    }
+
+    @Test
+    public void testAppendStringToName() {
+        product.appendStringToName("12");
+        assertEquals("iphone12", product.getName());
+    }
+}
